bc0ad172281970cea7ca90ed685e826feccb03ec
Jazmin M <jazminmerckel@gmail.com>
2019-12-04 03:20:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
    GROUP BY
     projects.id
    HAVING
-    SUM(pledges.amount) > funding_goal;"
+    SUM(pledges.amount) >= funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
