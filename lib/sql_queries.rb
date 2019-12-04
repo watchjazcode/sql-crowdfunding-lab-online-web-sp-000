1b9e095a83b5491b4c25e3729378d29c80ecce18
Jazmin M <jazminmerckel@gmail.com>
2019-12-04 03:04:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT name, age, pledges.amount FROM users, pledges WHERE users.id = pledges.user_id ORDER BY name ASC;"
+  "SELECT name, age, SUM(pledges.amount) FROM users, pledges WHERE users.id = pledges.user_id GROUP BY users.id ORDER BY name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
