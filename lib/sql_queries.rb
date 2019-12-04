9f195a8f42b72fe42f345ecd066394c4dc9c7b7b
Jazmin M <jazminmerckel@gmail.com>
2019-12-04 03:39:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,9 +41,24 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT
+    category, amount
+   FROM
+    projects, pledges
+   WHERE
+    projects.id = pledges.project_id AND 
+    projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT
+    name, SUM(amount)
+   FROM
+    users, pledges
+   WHERE
+    users.id = pledges.user_id
+   GROUP BY
+    name
+   ORDER BY
+     SUM(amount)"
 end
